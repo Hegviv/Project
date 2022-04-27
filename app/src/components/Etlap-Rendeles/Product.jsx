1daf2bc75caf7a81a8e70ef6b8f4c3691a576092
Hegviv <71432135+Hegviv@users.noreply.github.com>
2022-04-27 20:36:02 +0200

    comm

--- app/src/components/Etlap-Rendeles/Product.jsx
+++ app/src/components/Etlap-Rendeles/Product.jsx
@@ -5,17 +5,21 @@ export default function Product(props) {
   return (
     <div >
       <div className='container'>
-        <div style={{marginRight:"20%"}} className='col-sm-3'>
+        <div style={{marginRight:"100px"}} className='col-sm-2'>
+      
       <img className="kep" src={product.image} alt={product.name}  />
       </div>
-      <div style={{marginRight:"20px"}} className='col-sm-3'>
+   
+      <div style={{marginRight:"20px"}} className='col-sm-5'>
       <h3>{product.name}</h3>
+      <p>{product.leiras}</p>
       </div>
-      <div style={{marginRight:"20px"}} className='col-sm-3'>
+      <div style={{marginRight:"20px"}} className='col-sm-2'>
 
-      {product.price}  Ft
+     
       </div>
-      <div style={{marginRight:"20px"}} className='col-sm-3'>
+      <div style={{marginRight:"20px"}} className='col-sm-2'>
+     <h3> {product.price}  Ft</h3>
         <button className='kosar' style={{color:"black", marginBottom:"20px"}} onClick={() => onAdd(product)}></button>
       </div>
       </div>
--- app/src/components/data.jsx
+++ app/src/components/data.jsx
@@ -2,21 +2,37 @@ const data = {
     products: [
       {
         id: '1',
-        name: 'Sushi',
-        price: 1400,
-        image: 'https://kep.cdn.index.hu/1/0/2914/29146/291467/29146717_2235663_5fce0042b6f71e8702669f93f6d575ab_wm.jpg',
+        name: 'Pad mee sua',
+        price: 1500,
+        image: 'https://hot-thai-kitchen.com/wp-content/uploads/2018/08/pad-mee-tiew-sm.jpg',
+        leiras:"Tészta, fokhagyma, tofu, káposzta, sárgarépa, shiitake gomba"
       },
       {
         id: '2',
-        name: 'Rámen',
-        price: 2400,
-        image: 'https://receptneked.hu/wp-content/uploads/2016/09/IMG_0701.jpg',
+        name: 'Pad Woon Sen',
+        price: 1490,
+        image: 'https://delishar.com/wp-content/uploads/2016/05/5-Pad-woon-sen-1.jpg',
+        leiras:"tészta, fokhagyma, tojás, csirek, Osztrigaszósz"
       },
       {
         id: '3',
-        name: 'Tavaszi tekercs',
-        price: 1000,
-        image: '',
+        name: 'Panang',
+        price: 1800,
+        image: 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/b65af6ca9a16994fc79a10bedef4a2eb/Derivates/5358dd4f68ecee8fc51f5c4459dc6db2bafa2d83.jpg',
+        leiras:"mogyoróvaj, csirkemell, kaliforniai paprika, gyömbér, fokhagyma, kókusztej, halszósz, rizs"
+
+      },
+      {
+        id: '4',
+        name: 'narancsos csirke',
+        price: 1820,
+        image: 'https://izeselet.hu/wp-content/uploads/2017/03/narancsos-csirke1.jpg',
+      },
+      {
+        id: '5',
+        name: 'Ropogós, szószos narancsos csirke',
+        price: 1600,
+        image: 'https://izeselet.hu/wp-content/uploads/2016/11/thai-leves.jpg',
       },
     ],
   };
